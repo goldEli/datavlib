8dc506012d8a4310ce874a624a869c36f4f32293
miaoyu <miaoyu@sefonsoft.com>
2021-01-14 13:50:08 +0800

    svg viewbox

--- src/test.vue
+++ src/test.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="test">{{count}}</button>
+  <svg width="100%" height="100%" viewBox="0 0 100 100">
+    <line x1="0" y1="50" x2="100" y2="50" stroke-width="8" stroke="currentColor" />
+    <line x1="50" y1="0" x2="50" y2="100" stroke-width="8" stroke="currentColor" />
+  </svg>
 </template>
 <script>
 import { ref } from 'vue'
@@ -14,7 +17,4 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.test {
-  color: red;
-}
 </style>
