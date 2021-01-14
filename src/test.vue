35af41d63e20d4b3c9dc5c21cce4303d519e56b2
miaoyu <miaoyu@sefonsoft.com>
2021-01-14 14:29:40 +0800

    svg-more-icon

--- src/test.vue
+++ src/test.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    <svg width="100%" height="100%" viewBox="0 0 100 100">
-      <line x1="0" y1="50" x2="100" y2="50" stroke-width="8" stroke="currentColor" />
-      <line x1="50" y1="0" x2="50" y2="100" stroke-width="8" stroke="currentColor" />
-    </svg>
-    <svg width="500" height="200" viewBox="0 0 50 20" style="border: 1px solid #000000">
-      <rect x="20" y="10" width="10" height="5" style="stroke:#000000; fill: none" />
-    </svg>
-    <svg 
-      width="500" 
-      height="200" 
-      viewBox="0 0 200 200" 
-      style="border: 1px solid #000000"
-      preserveAspectRatio="xMinYMid meet"
-    >
-      <rect x="200" y="100" width="100" height="50" stroke-width="10" style="stroke:#000000; fill: none" />
+    <svg width="0" height="0">
+      <defs>
+        <symbol id="more" viewBox="0 0 100 100">
+          <circle r="5" cx="20" cy="25" fill="currentColor" />
+          <circle r="5" cx="20" cy="50" fill="currentColor" />
+          <circle r="5" cx="20" cy="75" fill="currentColor" />
+          <line x1="40" y1="25" x2="90" y2="25" stroke-width="8" stroke="currentColor" />1
+          <line x1="40" y1="50" x2="90" y2="50" stroke-width="8" stroke="currentColor" />1
+          <line x1="40" y1="75" x2="90" y2="75" stroke-width="8" stroke="currentColor" />1
+        </symbol>
+      </defs>
     </svg>
   </div>
 </template>
